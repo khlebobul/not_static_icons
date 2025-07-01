f0028047c54b94b77d1e31d2a084172fdff69177
Gleb Shalimov <sbgleb10@gmail.com>
2025-07-01 14:36:25 +0300

    add app window_mac_icon

--- example/not_static_icons_app/lib/data/icons_data.dart
+++ example/not_static_icons_app/lib/data/icons_data.dart
@@ -49,6 +49,7 @@ import '../icons/annoyed_icon.dart';
 import '../icons/antenna_icon.dart';
 import '../icons/anvil_icon.dart';
 import '../icons/aperture_icon.dart';
+import '../icons/app_window_mac_icon.dart';
 
 class IconData {
   final String name;
@@ -83,6 +84,7 @@ final List<IconData> icons = [
   IconData(name: 'antenna', widget: AntennaIcon(size: 40)),
   IconData(name: 'anvil', widget: AnvilIcon(size: 40)),
   IconData(name: 'aperture', widget: ApertureIcon(size: 40)),
+  IconData(name: 'app-window-mac', widget: AppWindowMacIcon(size: 40)),
   IconData(name: 'align-center', widget: AlignCenterIcon(size: 40)),
   IconData(name: 'align-left', widget: AlignLeftIcon(size: 40)),
   IconData(name: 'align-right', widget: AlignRightIcon(size: 40)),
